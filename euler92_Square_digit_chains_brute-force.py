d8c4b2983d2afcbe8fe5d641677af080b662b032
mike0618 <mishau7@gmail.com>
2021-03-22 13:55:24 +0300

    Initial commit

---
+++ euler92_Square_digit_chains_brute-force.py
@@ -0,0 +1,35 @@
+# Square digit chains
+from time import time
+from tqdm import tqdm
+
+start = time()
+
+
+def flight(s):
+    return sum(int(c) ** 2 for c in s)
+
+
+def e92():
+    nums89 = {89}
+    nums1 = {1}
+    for n in tqdm(range(2, 10000000)):
+        if n in nums1 or n in nums89:
+            continue
+        nums = {n}
+        while True:
+            num = flight(str(n))
+            if num in nums89:
+                nums89.update(nums)
+                break
+            elif num in nums1:
+                nums1.update(nums)
+                break
+            else:
+                n = num
+                nums.add(num)
+    return len(nums89)
+
+
+if __name__ == "__main__":
+    print('There are', e92(), 'numbers arrived to 89')  # 8581146
+    print('Runtime =', time() - start)
