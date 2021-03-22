d67c732358c2fee9240ba5590227862e568316db
mike0618 <mishau7@gmail.com>
2021-03-22 09:49:57 +0300

    Initial commit

---
+++ euler62_Cubic_permutations_fast.py
@@ -0,0 +1,22 @@
+# Cubic permutations
+from time import time
+
+start = time()
+
+
+def e62(n):
+    d = dict()
+    i = 0
+    while True:
+        i += 1
+        cube = i ** 3
+        value = ''.join(sorted(str(cube)))
+        d[value] = d.get(value, []) + [cube]
+        if len(d[value]) == n:
+            # print(d)
+            return min(d[value])
+
+
+if __name__ == "__main__":
+    print('The smallest cube =', e62(5))  # 127035954683
+    print('Runtime =', time() - start)
